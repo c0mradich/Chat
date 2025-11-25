8a4518171fbfcf1b35bd1a9c0f71f00e43892a33
c0mradich <bazankiril@gmail.com>
2025-11-25 21:10:29 +0100

    FINISHED

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,6 @@ from stuff import app, socketio
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()
         db.create_all()
         User.query.update({User.isActive: False})
         db.session.commit()
