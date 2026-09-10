451471300ef25e57acc7b9da0f9d1156d05447d9
c0mradich <bazankiril@gmail.com>
2026-09-10 18:55:34 +0200

    Fix Render deployment

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
-from flask import Flask
 from stuff.db import db, User
 from stuff import app, socketio
-
+import os
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         User.query.update({User.isActive: False})
         db.session.commit()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+        socketio.run(
+            app,
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 5000)),
+            debug=False,
+            allow_unsafe_werkzeug=True
+        )
